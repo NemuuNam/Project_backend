2866b466797dbd57611201053690ef65f7f8817c
thitiyaporn-s-tech <thitiyaporn-s@rmutp.ac.th>
2026-01-15 01:07:45 +0700

    fix cors 3

--- src/server.js
+++ src/server.js
@@ -35,26 +35,18 @@ const allowedOrigins = [
   'https://project-frontend-pi-sandy.vercel.app', //URL ของ Frontend ที่โฮสต์บน Vercel
 ];
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://project-frontend-pi-sandy.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  // ถ้าเป็น OPTIONS ให้ตอบกลับ 200 OK ทันที
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(null, false); 
+    }
+  },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 app.use(express.json());
 
 // ==========================================
